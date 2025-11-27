801a9bd9e29be61bf1223dccb29f677cf66da7cd
Sonal Karmakar <234934724+sonalkarmakar@users.noreply.github.com>
2025-11-27 07:52:10 +0530

    Created Test.java
    Created the Test.java file to contain the main code.

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test {
+	static void add()
+	{
+		int a = 50;
+		int b = 10;
+		int res = a + b;
+		System.out.println("The result is " + res);
+	}
+	public static void main(String[] args)
+	{
+		add();
+	}
+}
